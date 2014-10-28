b49d1899496b570ff7343c1870d4fa6277c0bf11
fet mar <fetmar@gmail.com>
2014-10-28 10:30:51 -0400

    refactor due to total_rows nonsense

--- helpers/CouchIterator.rb
+++ helpers/CouchIterator.rb
@@ -1,3 +1,4 @@
+require_relative "../utilities/cloneDeep.rb"
 
 class CouchIterator
 
@@ -7,36 +8,43 @@ class CouchIterator
     @chunkSize      = options[:chunkSize] || 500
     @index          = options[:index]     || 0
     @method         = options[:method]    || "get"
-    @totalRows      = nil
+    @noMoreRows     = false
   end
 
-  def refreshTotalRows
-    refreshOptions = @requestOptions.merge( { :params => { :limit => 1, :refresher => true } } )
-    return @totalRows = doRequest(refreshOptions)['total_rows']
-  end
 
   def hasChunk()
-    if @chunkSize * @index < @totalRows
-      return true
-    else
-      return false
-    end
+    return ! @noMoreRows
   end
 
   def getChunk()
+
     requestIndex = @chunkSize * @index
-    chunkyOptions = @requestOptions.merge( { :params => { :limit => @chunkSize, :skip => requestIndex } } )
+
+    chunkyOptions = cloneDeep(@requestOptions)
+    chunkyOptions[:params] ||= {}
+    chunkyOptions[:params]["limit"] = @chunkSize
+    chunkyOptions[:params]["skip"]  = requestIndex
+    
     response = doRequest(chunkyOptions)
+
+    @noMoreRows = response['rows'].length == 0
+
     @index += 1
+
     return response
   end
 
   def doRequest(options)
+
     if @method == "get"
-      return JSON.parse(@couch.getRequest(options).to_json)
+      result = @couch.getRequest(options)
     elsif @method == "post"
-      return JSON.parse(@couch.postRequest(options).to_json)
+      result = @couch.postRequest(options)
     end
+
+    return JSON.parse(result) if result.class == String
+    return result
+
   end
 
 end
--- helpers/CouchIteratorTest.rb
+++ helpers/CouchIteratorTest.rb
@@ -1,31 +1,36 @@
-require './config.rb'
-require './Couch.rb'
-
+require '../config.rb'
+require_relative 'Couch'
+require_relative 'CouchIterator'
 
 couch = Couch.new({
-  :cache     => false,
   :host      => $settings[:host],
   :login     => $settings[:login],
-  :designDoc => $settings[:designDoc],
-  :db        => 'group-fake_tutor',
-  :local     => $settings[:local]
+  :designDoc => 'tangerine',
+  :db        => 'group-tutor_feb_25'
 })
 
 
 requestOptions = {
-  :view => "resultsByWorkflowId",
-  :data => { "keys" => ["c835fc38-de99-d064-59d3-e772ccefcf7d"] },
-  :params => { "limit" => 1 }
+  :view => "tutorTrips",
+  :data => { "keys" => ["workflow-c835fc38-de99-d064-59d3-e772ccefcf7d"] }
 }
 
 iterator = CouchIterator.new({
   :couch          => couch,
   :requestOptions => requestOptions,
-  :chunkSize      => 621,
+  :chunkSize      => 5000,
   :method         => 'post'
 })
 
-puts iterator.refreshTotalRows()
-puts iterator.hasChunk()
-puts iterator.getChunk()
-puts iterator.hasChunk()
+tStart = Time.now.to_f
+puts "starting"
+while iterator.hasChunk()
+
+  result = iterator.getChunk()
+  puts "#{result.to_s.length} characters returned"
+end
+
+puts "Test. #{"%.3f" % (Time.now.to_f - tStart)}"
+
+puts "last one"
+puts result
