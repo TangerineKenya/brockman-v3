abe21f76cb137157e4649f4a295df9e1f2f1990f
fet mar <fetmar@gmail.com>
2015-02-02 17:18:52 +0000

    remove costly methods

--- cron.rb
+++ cron.rb
@@ -28,10 +28,12 @@ END
 
 puts header
 
-dbs = [ 'tangent_12m' ]
-years  = [ 2013, 2014 ]
+dbs = [ 'tangent_data_1m_baringo', 'group-national_tablet_program' ]
+years  = [ 2013, 2014, 2015 ]
 months = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
 workflowIds = ["00b0a09a-2a9f-baca-2acb-c6264d4247cb","c835fc38-de99-d064-59d3-e772ccefcf7d"]
+
+CHUNK_SIZE = 1000
 
 dbs.each { |db|
 
@@ -44,27 +46,36 @@ dbs.each { |db|
     :db        => db
   })
 
-  tripIds = []
-    workflowIds.each { |workflowId|
+  puts "Caching trips: "
+
+  workflowIds.each { |workflowId|
     tripsRequest = JSON.parse(couch.postRequest({
       :view => "tutorTrips",
       :params => {"reduce" => false},
       :data => {"keys" => ["workflow-#{workflowId}"]}
     }))
-    tripIds += tripsRequest['rows'].map{|row| row['value']}
+    puts "Processing Workflow: #{workflowId}"
+    hTripIds = {}
+    tripsRequest['rows'].each { |row| hTripIds[row['value']] = true}
+    aTripIds = hTripIds.keys
+    totalTripIds = aTripIds.length
+   
+    
+
+    (0..totalTripIds).step(CHUNK_SIZE).each { | chunkIndex |
+      idChunk = aTripIds.slice(chunkIndex, chunkIndex + CHUNK_SIZE)
+      couch.postRequest({
+        :view   => "spirtRotut",
+        :params => { "group" => true },
+        :cache  => true,
+        :data   => { "keys" => idChunk }
+      })
+      print "M"
+    }
+
   }
 
-  tripIds = tripIds.uniq
-
-  puts "Trips to cache :#{tripIds.length}"
-
-  couch.postRequest({
-    :view      => "spirtRotut",
-    :params    => { "group" => true },
-    :cache     => true,
-    :data      => { "keys" => tripIds }
-  })
-
+  
   puts "view: tutorTrips"
 
   years.each { |year| 
