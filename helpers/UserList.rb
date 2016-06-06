d14e93f4c4999eecd792c786f8c6aceed18fecf1
Matt Finholt-Daniel <mfinholt@rti.org>
2016-06-06 09:09:10 -0400

    Include MPESA number in CSV report

--- helpers/UserList.rb
+++ helpers/UserList.rb
@@ -1,50 +1,31 @@
 # encoding: utf-8
 
 
-class LocationList
+class UserList
 
   def initialize( options )
     @couch = options[:couch]
-    @locationListDoc = @couch.getRequest({ 
-      :doc => "location-list", 
-      :parseJson => true 
+    @userDocs = @couch.getRequest({
+      :doc => "_all_docs",
+      :params => { 
+        "startkey" => "user-".to_json,
+        "include_docs" => true
+      },
+      :parseJson => true
     })
-
-    @locationIndex ||= {}
-
-    indexLocations(@locationListDoc["locations"], 0, [])
+    @userIndex ||= {}
+    indexUsers()
   end
 
-  def indexLocations( node, depth, locMap )
-    node.map { | id, loc | 
-      locMap[depth] = id
-      if loc.has_key?("children")
-        indexLocations(loc["children"], depth+1, locMap)
-      else
-        @locationIndex[id] = [] | locMap
-      end
+  def indexUsers()
+    @userDocs['rows'].map{ | user | 
+      username              = user['doc']['name']
+      @userIndex[username]  = user['doc']
     }
   end
 
-  def retrieveLocation( key )
-    treePath = @locationIndex[key]
-    if treePath
-      location = walkTree(@locationListDoc["locations"], 0, treePath)
-      return location
-    end
-    return {}
+  def getUser( username )
+    return (@userIndex[username] || {})
   end 
-
-  def walkTree(node, depth, path)
-    if node[path[depth]]
-      nodeData = node[path[depth]].select {|k,v| (k != 'id' and k != "children") }
-      nodeData = Hash[nodeData.map {|k,v| ["#{titleize(@locationListDoc['locationsLevels'][depth])}#{titleize(k)}", v] }].to_a
-      if (depth+1) < path.length
-        nodeData = nodeData | walkTree(node[path[depth]]["children"], depth+1, path)
-      end
-      return nodeData
-    end
-    return []
-  end
 
 end
